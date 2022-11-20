395c5006428b4f15a59bdbbb7085d6771bc3accf
michaelwaves <68243408+michaelwaves@users.noreply.github.com>
2022-11-20 09:34:20 -0500

    fixed animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
     public float horizontalInput;
     public int points = 0;
+    public float forceStrength = 10f;
 
     public TextMeshProUGUI pointText;
 
@@ -21,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        rb.AddForce(Vector3.left * horizontalInput*10);
+        rb.AddForce(Vector3.left * horizontalInput*forceStrength);
     }
 
     void OnTriggerEnter(Collider other)
